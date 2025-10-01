76bda6c9817bda76132825c120cc19367d334d90
fedog-ops <felixtmcd@gmail.com>
2025-10-01 16:19:26 +0100

    Refactor ticket icons to use Lucide components for improved scalability and maintainability

---
+++ src/features/ticket/constants.tsx
@@ -0,0 +1,7 @@
+import { LucideCircleCheck, LucidePencil, LucideFileText } from "lucide-react";
+
+export const TICKET_ICONS = {
+    OPEN: <LucideFileText />,
+    IN_PROGRESS: <LucidePencil />,
+    DONE: <LucideCircleCheck />,
+}
