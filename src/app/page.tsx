395b528bf6717a0d09cf56d63234196d60238b06
fedog-ops <felixtmcd@gmail.com>
2025-10-01 16:19:57 +0100

    Add Heading component for improved page structure and semantics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import { ticketsPath } from "../paths";
-
+import Heading from "../components/heading";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">Welcome to the Home Page</h2>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <Heading title="Home" description="Welcome to the Home Page" />
       <Link href={ticketsPath()} className="underline">Go to Tickets</Link>
     </div>
   );
