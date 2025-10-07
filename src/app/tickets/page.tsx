7bb84c16c54275797f5f3c47d31b3a5688f16231
fedog-ops <felixtmcd@gmail.com>
2025-10-07 17:57:58 +0100

    retrieve tickets in a server side component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,19 +1,18 @@
-import { initialTickets } from '../../data';
 import Heading from "../../components/heading";
-import { TicketItem } from "../../features/ticket/components/ticket-item";
+import {Suspense} from "react";
+import {TicketList} from "@/features/ticket/components/ticket-list";
+import {Spinner} from "@/components/spinner";
 
-const Tickets = () => {
+const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
            <Heading title="TicketsPage" description="All your tickets in one place!" />
-            <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
-                {initialTickets.map(ticket => (
-                   <TicketItem key={ticket.id} ticket={ticket} />
-                ))}
 
-            </div>
+            <Suspense fallback={<Spinner />}>
+                <TicketList />
+            </Suspense>
         </div>
     );
 }
 
-export default Tickets;
+export default TicketsPage;
