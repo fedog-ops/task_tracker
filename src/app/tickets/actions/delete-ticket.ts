4388cc6c215049aa2c55c6bfd7496ee2014dab2a
fedog-ops <felixtmcd@gmail.com>
2025-10-09 09:28:28 +0100

    implementing on demand caching

--- src/app/tickets/actions/delete-ticket.ts
+++ src/app/tickets/actions/delete-ticket.ts
@@ -3,12 +3,14 @@
 import { redirect } from "next/navigation";
 import { prisma } from "../../../lib/prisma";
 import { ticketsPath } from "../../../paths";
+import { revalidatePath } from "next/cache";
 
 const deleteTicket = async (id: string) => {
     await prisma.ticket.delete({
         where: { id }
     });
 
+    revalidatePath(ticketsPath());
     redirect(ticketsPath());
 }
 
--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,8 +3,6 @@ import {Suspense} from "react";
 import {TicketList} from "@/features/ticket/components/ticket-list";
 import {Spinner} from "@/components/spinner";
 
-export const dynamic = 'force-dynamic';
-
 const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
