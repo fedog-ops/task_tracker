634d70c3154e0bf0a429c1d554d278341afb1c04
fedog-ops <felixtmcd@gmail.com>
2025-10-09 10:55:41 +0100

    configure card box instead of compose

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -3,22 +3,20 @@ import { Suspense } from "react";
 import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Spinner } from "@/components/spinner";
 import { TicketCreateForm } from "../../features/ticket/components/create-ticket-form";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../../components/ui/card";
+import { CardCompact } from "../../components/card-compact";
 
 const TicketsPage = () => {
     return (
         <div className="flex-1 flex flex-col gap-y-8">
             <Heading title="TicketsPage" description="All your tickets in one place!" />
 
-            <Card className="w-full max-w-[420px] self-center">
-                <CardHeader>
-                    <CardTitle>Create Ticket</CardTitle>
-                    <CardDescription>A new ticket will be created</CardDescription>
-                </CardHeader>
-                <CardContent>
-                    <TicketCreateForm />
-                </CardContent>
-            </Card>
+            <CardCompact 
+                title="Create Ticket" 
+                description="A new ticket will be created" 
+                className="w-full max-w-[420px] self-center" 
+                content={<TicketCreateForm />} 
+            />
+               
             <Suspense fallback={<Spinner />}>
                 <TicketList />
             </Suspense>
