627b040ab458b1842de9d7348044c9d5818084ce
fedog-ops <felixtmcd@gmail.com>
2026-01-09 15:43:13 +0000

    Form Validation

--- src/app/tickets/actions/upsert-ticket.ts
+++ src/app/tickets/actions/upsert-ticket.ts
@@ -3,23 +3,33 @@
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
+import { z } from "zod";
 import { ticketPath, ticketsPath } from "@/paths";
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData
 ) => {
-  const data = {
-    title: formData.get("title") as string,
-    content: formData.get("content") as string,
-  };
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get("title") as string,
+      content: formData.get("content") as string,
+    });
 
-  await prisma.ticket.upsert({
-    where: { id: id || "" },
-    update: data,
-    create: data,
-  });
+    await prisma.ticket.upsert({
+      where: { id: id || "" },
+      update: data,
+      create: data,
+    });
+  } catch (error) {
+    return { message: "There was an error processing your request" };
+  }
 
   revalidatePath(ticketsPath());
 
@@ -27,5 +37,5 @@ export const upsertTicket = async (
     redirect(ticketPath(id));
   }
 
-  return { message: "Ticket created successfully." };
+  return { message: "Ticket created successfully" };
 };
